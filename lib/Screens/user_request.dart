a3170e7a3a43b0ed7023973da13514cf7c5f0eec
Etch <hashem_khaled33@yahoo.com>
2020-10-07 13:28:28 +0200

    user request with Photos options

--- lib/Screens/user_request.dart
+++ lib/Screens/user_request.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:multi_image_picker/multi_image_picker.dart';
 import 'package:talabatk_flutter/Entities/global.dart';
 import 'package:talabatk_flutter/Entities/user.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -16,12 +20,17 @@ class UserRequest extends StatefulWidget
 }
 
 class _State extends State<UserRequest>{
+  File _image;
+  final picker = ImagePicker();
+  List<Asset> images = List<Asset>();
+  String _error = 'No Error Dectected';
   User shop;
   _State(this.shop);
 
   @override
   Widget build(BuildContext context) {
           return Scaffold(
+            resizeToAvoidBottomPadding: false,
               appBar:AppBar(
                 backgroundColor: Color(int.parse(Global.primaryColor)),
                 centerTitle: true,
@@ -34,34 +43,227 @@ class _State extends State<UserRequest>{
                 automaticallyImplyLeading: false,
               ),
             body: Padding(
-              padding: EdgeInsets.all(10),
+              padding: EdgeInsets.all(20),
               child: Column(
 
                 children: [
+                  Center(
+                    child: Text("تفاصيل الطلب",
+                      style: TextStyle(
+                        color: Color(int.parse(Global.primaryColor)),
+                        fontFamily: Global.fontFamily,
+                        fontWeight: FontWeight.w900,
+                        fontSize: 18),),
+                  ),
                   Container(
-                      height: 50,
-                      child: RaisedButton(
-                          onPressed: () {
-                            launch('tel://${shop.mobileNumber}');
-                          },
-                          elevation: 2.0,
-                          color: Color(int.parse(Global.primaryColor)),
-                          textColor: Colors.white,
-                          padding: const EdgeInsets.all(0.0),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
+
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        borderOnForeground: true,
+                        elevation: 10,
+                        child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: TextField(
+                            maxLines: 10,
+                            decoration: InputDecoration.collapsed(hintText: ""),
                           ),
-                          child: Container(
-                            padding: const EdgeInsets.all(10.0),
-                            child: const Text('اتصال  ',
-                                style: TextStyle(fontSize: 20)
+                        ),
+                      ) ),
+                  SizedBox(height: 10),
+                  Expanded(
+                    child: buildGridView(),
+                  ),
+                  Container(
+                    width: 80,
+                    height: 80,
+                    child:  Center(
+                      child: _image == null
+                          ? Text('')
+                          : Image.file(_image),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                     mainAxisAlignment: MainAxisAlignment.center ,//Center Row contents horizontally,
+                    children: [
+                      Container(
+
+                          child: Center(
+                            child: SizedBox.fromSize(
+                              size: Size(80, 80), // button width and height
+                              child: ClipOval(
+                                child: Material(
+                                  color:  Color(int.parse(Global.primaryColor)), // button color
+                                  child: InkWell(
+                                    splashColor: Color(int.parse(Global.secondaryColor)), // splash color
+                                    onTap: () {
+                                      launch('tel://${shop.mobileNumber}');
+                                    }, // button pressed
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: <Widget>[
+                                        Icon(Icons.call,color: Colors.white,), // icon
+                                        Text("اتصال"  , style: TextStyle(
+                                        color: Colors.white,
+                                   ),), // text
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                          )),
+                      SizedBox(width: 20),
+                      Container(
+
+                          child: Center(
+                            child: SizedBox.fromSize(
+                              size: Size(80, 80), // button width and height
+                              child: ClipOval(
+                                child: Material(
+                                  color:  Color(int.parse(Global.primaryColor)), // button color
+                                  child: InkWell(
+                                    splashColor: Color(int.parse(Global.secondaryColor)), // splash color
+                                    onTap: () {
+                                      _showPicker(context);
+                                    }, // button pressed
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: <Widget>[
+                                        Icon(Icons.add_a_photo,color: Colors.white,), // icon
+                                        Text("   رفع صوره", style: TextStyle(
+                                          color: Colors.white,
+                                        )), // text
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                          )),
+                    ],
+                  ),
+                  SizedBox(height: 50),
+                  Container(
+
+                      child: Center(
+                        child: RaisedButton(
+                            onPressed: () {
+
+                            },
+                            elevation: 2.0,
+                            color: Color(int.parse(Global.primaryColor)),
+                            textColor: Colors.white,
+                            padding: const EdgeInsets.all(0.0),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
                             ),
-                          )
-                      ),),
+                            child: Container(
+                              padding: const EdgeInsets.all(10.0),
+                              child: const Text('طلب اوردر',
+                                  style: TextStyle(fontSize: 20)
+                              ),
+                            )
+                        ),
+                      )),
+
+
+
+
                 ],
               ),
             ),
           );
   }
 
+
+
+  Widget buildGridView() {
+    return GridView.count(
+      crossAxisCount: 3,
+      children: List.generate(images.length, (index) {
+        Asset asset = images[index];
+        return AssetThumb(
+          asset: asset,
+          width: 100,
+          height: 100,
+        );
+      }),
+    );
+  }
+
+  Future<void> loadAssets() async {
+    List<Asset> resultList = List<Asset>();
+    String error = 'No Error Dectected';
+
+    try {
+      resultList = await MultiImagePicker.pickImages(
+        maxImages: 3,
+       // enableCamera: true,
+        selectedAssets: images,
+        cupertinoOptions: CupertinoOptions(takePhotoIcon: "chat"),
+        materialOptions: MaterialOptions(
+          actionBarColor: "#abcdef",
+          actionBarTitle: "Example App",
+          allViewTitle: "All Photos",
+          useDetailsView: false,
+          selectCircleStrokeColor: "#000000",
+        ),
+      );
+    } on Exception catch (e) {
+      error = e.toString();
+    }
+
+
+    if (!mounted) return;
+
+    setState(() {
+      images = resultList;
+      _error = error;
+    });
+  }
+  void _showPicker(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext cont) {
+          return SafeArea(
+            child: Container(
+              child: new Wrap(
+                children: <Widget>[
+                    new ListTile(
+                        leading: new Icon(Icons.photo_library),
+                        title: new Text('Gallery'),
+                        onTap: () {
+                          loadAssets();
+                          Navigator.of(context).pop();
+                        }),
+                    new ListTile(
+                      leading: new Icon(Icons.photo_camera),
+                      title: new Text('Camera'),
+                      onTap: () {
+                        getImage(ImageSource.camera);
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                ],
+              ),
+            ),
+          );
+        }
+    );
+  }
+  //Select an Image via gallery or camera
+  Future getImage(ImageSource source) async {
+    final pickedFile = await picker.getImage(source: source);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
 }
