4b0488247fcc105199fd589bba68849161112a06
MostafaBayuome <mostafabayome38@gmail.com>
2020-10-15 11:01:54 +0200

    commit14/10/2020 TabsLayout

--- lib/Screens/RequestsLayout.dart
+++ lib/Screens/RequestsLayout.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:talabatk_flutter/Entities/global.dart';
 import 'package:talabatk_flutter/Screens/user_request.dart';
 
-class RequestsLayout extends StatelessWidget {
-  String title="الطلبات تحت التنفيذ";
+class RequestsLayout extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+class _State extends State<RequestsLayout> {
+  String _title="الطلبات";
    BuildContext currContest=null;
   @override
   Widget build(BuildContext context) {
@@ -15,13 +19,14 @@ class RequestsLayout extends StatelessWidget {
       home: DefaultTabController(
         length: 4,
         child: new Scaffold(
+
           appBar: AppBar(
             backgroundColor: Color(int.parse(Global.primaryColor)),
             centerTitle: true,
-            title:Text(title,
+            title:Text(_title,
               style: TextStyle(
                   fontFamily: Global.fontFamily,
-                  fontWeight: FontWeight.w900,
+                  fontWeight: FontWeight.w600,
                   fontSize: 25
               ),),
             automaticallyImplyLeading: false,
@@ -30,20 +35,20 @@ class RequestsLayout extends StatelessWidget {
             children: [
               new Container(
                 color: Colors.yellow,
-                 child: WaitedRequests(Icons.timer,'Waited'),
+                 child: WaitedRequests(Icons.timer,'تحت الانتظار',0),
               ),
               new Container(
                 color: Colors.orange,
-                child: WaitedRequests(Icons.two_wheeler,'Processing'),
+                child: WaitedRequests(Icons.two_wheeler,'جاري التنفيذ',1),
               ),
               new Container(
-
+                
                 color: Colors.lightGreen,
-                child: WaitedRequests(Icons.done,'Delevired'),
+                child: WaitedRequests(Icons.done,'تم توصيلها',2),
                   ),
               new Container(
                 color: Colors.red,
-                child: WaitedRequests(Icons.cancel_rounded,'Rejected'),
+                child: WaitedRequests(Icons.cancel_rounded,'مرفوضة',3),
               ),
             ],
           ),
@@ -72,34 +77,74 @@ class RequestsLayout extends StatelessWidget {
     );
   }
 
-  WaitedRequests(IconData icon,String title) {
+  WaitedRequests(IconData icon,String title,int state) {
+
+  setState(){
+    _title=title;
+  }
 
   return  ListView(
 
-        children: <Widget>[
+          children: <Widget>[
 
-          Container(
+            Container(
 
-              child:  InkWell
-                (
-                onTap:(){
-                  Navigator.of(currContest).push(MaterialPageRoute(
-                      builder: (context) =>UserRequest()
-                  ));
-                },
-                child:Card(
+                child:  InkWell
+                  (
+                  onTap:(){
+                    Navigator.of(currContest).push(MaterialPageRoute(
+                        builder: (context) =>UserRequest()
+                    ));
+                  },
+                  child:Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          //mainAxisSize: MainAxisSize.min,
+
+                          children: <Widget>[
+                            ListTile(
+                              title: Text('Order number : 0111123455'),
+                              subtitle: Text('User Description  need 1 litre milk 10 eggs etc ......... plus new things will be added  plus new things will be added  '),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center ,
+                              children: <Widget>[
+                                FlatButton(
+                                  child: IconButton(
+                                    tooltip:'waited',
+                                    icon: Icon(icon),
+                                    color: Colors.red,
+
+                                    onPressed: () {
+                                      // here we send location of user
+                                    },
+                                  ),
+                                ),
+                                Text(title)
+                              ],
+                            )
+
+                          ],
+                        ),
+                      ) ) ,
+                )
+            ) ,
+            Container(
+
+                child:  Card(
                     child: Padding(
                       padding: const EdgeInsets.all(10.0),
                       child: Column(
-                        //mainAxisSize: MainAxisSize.min,
-
+                        mainAxisSize: MainAxisSize.min,
                         children: <Widget>[
                           ListTile(
-                            title: Text('Order number : 0111123455'),
-                            subtitle: Text('User Description  need 1 litre milk 10 eggs etc ......... plus new things will be added  plus new things will be added  '),
+                            title: Text('Order No : 01149056691'),
+                            subtitle: Text('User Description  i need to buy milk 100 orange asadas  ......... plus new things will be added  plus new things will be added  '),
                           ),
                           Row(
-                            mainAxisAlignment: MainAxisAlignment.center ,
+
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: <Widget>[
                               FlatButton(
                                 child: IconButton(
@@ -118,45 +163,16 @@ class RequestsLayout extends StatelessWidget {
 
                         ],
                       ),
-                    ) ) ,
-              )
-          ) ,
-          Container(
-              
-              child:  Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        ListTile(
-                          title: Text('Order No : 01149056691'),
-                          subtitle: Text('User Description  i need to buy milk 100 orange asadas  ......... plus new things will be added  plus new things will be added  '),
-                        ),
-                        Row(
+                    ) )
+            )
+          ]
+      );
 
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            FlatButton(
-                              child: IconButton(
-                                tooltip:'waited',
-                                icon: Icon(icon),
-                                color: Colors.red,
+  }
 
-                                onPressed: () {
-                                  // here we send location of user
-                                },
-                              ),
-                            ),
-                            Text(title)
-                          ],
-                        )
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
 
-                      ],
-                    ),
-                  ) )
-          )
-        ]
-    );
   }
 }
